eb449c5881c164254e64c55bdbdd495f3fdfe9b3
Sarthak Srivastava <sarthak.prof@gmail.com>
2021-04-30 18:18:49 +0530

    updated for special variables

--- cli-args.sh
+++ cli-args.sh
@@ -6,3 +6,13 @@ echo "Second Parameter : $2"
 echo "Quoted Values: $@"
 echo "Quoted Values: $*"
 echo "Total Number of Parameters : $#"
+
+# input: sh cli-args.sh Zara ALi
+
+# below output is example of special variable
+echo "below output is example of special variable"
+
+for TOKEN in $*
+do
+   echo $TOKEN
+done
