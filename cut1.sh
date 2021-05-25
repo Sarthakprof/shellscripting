b760781fc73b3cccf7ef1be3f7bdbe77c54bf23d
Sarthak Srivastava <sarthak.prof@gmail.com>
2021-05-25 20:26:10 +0530

    added more cuts

--- cut1.sh
+++ cut1.sh
@@ -4,3 +4,18 @@ cut -c3 $1
 
 # print the 2nd and 7th character 
 cut -c2,7 $1
+
+# print range of characters between 2nd and 7th position 
+cut -c2-7 $1
+
+#to print fields
+#Input
+#1 A 1 B 1 C
+#2 D 2 E 2 F
+#3 G 3 H 3 I
+
+#OUTPUT
+#A
+#D
+#G
+cut -f-3 $1
